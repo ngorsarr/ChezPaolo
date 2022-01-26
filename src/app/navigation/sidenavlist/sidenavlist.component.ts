101eb7660ec05c544b61a8857c125f6cec9791e8
SARR Ngor <ngorsarr91@gmail.com>
2022-01-26 00:46:51 +0000

    side nav close

--- src/app/navigation/sidenavlist/sidenavlist.component.ts
+++ src/app/navigation/sidenavlist/sidenavlist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidenavlist',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavlistComponent implements OnInit {
 
+  @Output() closeSideNavigation = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onToggleClose() {
+    this.closeSideNavigation.emit();
+  }
 }
